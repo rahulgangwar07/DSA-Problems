7bfdbb88c5b037f1f71ea16f4172d000ab2ad491
DEII <rahulkurmi97194@gmail.com>
2026-06-03 21:57:35 +0530

    Left Rotated and Largest Sum Contiguous Subarray

---
+++ DSA/Problems/Array1/LargestSumContiguousSubarray.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Array
+{
+    internal class LargestSumContiguousSubarray
+    {
+        //Kadane’s Algorithm
+
+        //int[] arr = { -3, -2, -5, -4 };
+        //int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        //int[] arr = { 3, -4, 5, 4, -1, 7, -8 };
+
+        //int ans = LargestSumContiguousSubarray.LargestSum(arr);
+        //Console.Write("Largest Sum Contiguous Subarray " + ans);
+
+
+        //int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        public static int LargestSum(int[] arr)
+        {
+            int currSum = 0;
+            int maxSum = int.MinValue;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                currSum += arr[i];
+                maxSum = Math.Max(currSum, maxSum);
+                if (currSum < 0)
+                {
+                    currSum = 0;
+                }
+            }
+            return maxSum;
+        }
+
+        //public static int LargestSum(int[] arr)
+        //{
+        //    int maxSum = int.MinValue;
+
+        //    for (int i = 0; i < arr.Length - 1; i++)
+        //    {
+        //        int currSum = 0;
+        //        for (int j = i; j < arr.Length; j++)
+        //        {
+        //            currSum += arr[j];
+        //            maxSum = Math.Max(maxSum, currSum);
+
+        //        }
+        //    }
+
+        //    return maxSum;
+        //}
+
+
+    }
+}
+
---
+++ DSA/Problems/Array1/LeftRotateArraybyNPosition.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Array
+{
+    internal class LeftRotateArraybyNPosition
+    {
+
+        //int[] arr = { 1, 2, 3, 4, 5 };
+        //int n = 4;
+
+        //int[] ans = LeftRotateArraybyNPosition.RotateNPositionLeft(n, arr);
+        //for (int i = 0; i<arr.Length; i++)
+        //    Console.Write(" " + ans[i]);
+        public static int[] RotateNPositionLeft(int k, int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return new int[0];
+
+            k = k % nums.Length;
+
+            int index = 0;
+            while (index < k)
+            {
+                int val = nums[0];
+                for (int i = 0; i < nums.Length - 1; i++)
+                {
+                    nums[i] = nums[i + 1];
+                }
+                nums[nums.Length - 1] = val;
+                index++;
+            }
+
+            return nums;
+        }
+
+        //int[] arr = { 1, 2, 3, 4, 5 };
+
+        //int[] ans = LeftRotateArraybyNPosition.RotateOnePositionLeft(arr);
+        //for(int i=0;i<arr.Length;i++)
+        //    Console.Write(" " + ans[i]);
+        public static int[] RotateOnePositionLeft(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return new int[0];
+
+            int FirstElement = nums[0];
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                nums[i] = nums[i + 1];
+            }
+
+            nums[nums.Length - 1] = FirstElement;
+
+            return nums;
+        }
+
+    }
+}
+
