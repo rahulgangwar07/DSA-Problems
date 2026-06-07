75dc902590759960388d39392bbebb7d0b7c0b2f
DEII <rahulkurmi97194@gmail.com>
2026-06-07 15:50:12 +0530

    Merge Two Sorted Array

---
+++ DSA/Problems/Array1/MergeTwoSortedArrays.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Array
+{
+    internal class MergeTwoSortedArrays
+    {
+        //int[] arr = { 2,3,4 };
+
+        public static int[] MergeArray(int[] arr1, int[] arr2)
+        {
+            int a = 0, b = 0;
+            int len = arr1.Length + arr2.Length ;
+            int i = 0;
+            int[] arr = new int[len];
+
+            while (a < arr1.Length && b < arr2.Length)
+            {
+                if (arr1[a] < arr2[b])
+                {
+                    arr[i] = arr1[a];
+                    a++;
+                }
+                else
+                {
+                    arr[i] = arr2[b];
+                    b++;
+                }
+                i++;
+            }
+            while (a < arr1.Length)
+            {
+                arr[i] = arr1[a];
+                a++;
+                i++;
+            }
+
+            while (b < arr2.Length)
+            {
+                arr[i] = arr2[b];
+                b++;
+                i++;
+            }
+
+            return arr;
+        }
+    }
+}
+
