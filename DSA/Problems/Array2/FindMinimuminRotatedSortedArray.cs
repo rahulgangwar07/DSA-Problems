4071ccf6c164edff66ecc1090fa2a71feb088df4
DEII <rahulkurmi97194@gmail.com>
2026-06-24 22:57:49 +0530

    Find Minimum Rotated Sorted Array

---
+++ DSA/Problems/Array2/FindMinimuminRotatedSortedArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Array2
+{
+    internal class FindMinimuminRotatedSortedArray
+    {
+        //Problem 35. Find Minimum in Rotated Sorted Array
+
+        //int[] nums = [4, 5, 6, 7, 0, 1, 2];
+
+        //int ans = FindMinimuminRotatedSortedArray.MinimumRotated(nums);
+        //Console.Write("Min Element: " + ans);
+        public static int MinimumRotated(int[] arr)
+        {
+        //[4, 5, 6, 7, 0, 1, 2]
+            int start = 0;
+            int end = arr.Length - 1;
+            while (start < end)
+            {
+                int mid = (start + end) / 2;
+                if (arr[mid] > arr[end])
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid;
+                }
+            }
+            return start;
+        }
+
+    }
+}
