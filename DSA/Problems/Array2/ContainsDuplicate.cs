89da94d955fe29836d8351ad4a0433dbfa3e53ce
DEII <rahulkurmi97194@gmail.com>
2026-06-24 06:45:40 +0530

    Contains Duplicate

---
+++ DSA/Problems/Array2/ContainsDuplicate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Array
+{
+    internal class ValidPalindrome
+    {
+        //Given a string s, return true if it is a palindrome, or false otherwise.
+        //Input: "A man, a plan, a canal: Panama"
+        //Output: true
+        public static bool ContainsDuplicates(int[] arr)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (dic.ContainsKey(arr[i]))
+                    return true;
+
+                dic[arr[i]] = i;
+            }
+
+            return false;
+        }
+    }
+
+}
+
+
