95d9df94ece0a0fed9a0edaa6b30cb1369d8e912
DEII <rahulkurmi97194@gmail.com>
2026-07-07 21:09:35 +0530

    Search A 2D Matrix

---
+++ DSA/Problems/Array2/Searcha2DMatrix.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Array2
+{
+    internal class Searcha2DMatrix
+    {
+
+        //int[][] arr = [
+        //               [1,3,5,7],
+        //                 [10,11,16,20],
+        //                 [23,30,34,60]
+        //              ];
+        //int target = 34;
+
+        //bool ans = Searcha2DMatrix.SearchMatrix(arr, target);
+        //Console.Write("Found: " + ans);
+
+        public static bool SearchMatrix(int[][] matrix,int target)
+        {
+            int rows = matrix.Length;
+            int cols = matrix[0].Length;
+            int start = 0;
+            int end = (rows * cols) - 1;
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+                int row = mid / cols;
+                int col = mid % cols;
+
+                int value = matrix[row][col];
+                if(value == target)
+                {
+                    return true;
+                }
+                else if (value < target)
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+
+            return false;
+        }
+    }
+}
