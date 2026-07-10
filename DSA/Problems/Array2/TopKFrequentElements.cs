eca16192ba1092267f27e8559f26a7575c21c301
DEII <rahulkurmi97194@gmail.com>
2026-07-10 22:07:16 +0530

    Top k Frequent Elements

---
+++ DSA/Problems/Array2/TopKFrequentElements.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Array
+{
+    internal class TopKFrequentElements
+    {
+        //arr = [1, 1, 1, 2, 2, 3]
+        //k = 2
+        public static int[] CountFrequent(int k, int[] arr)
+        {
+            Dictionary<int, int> dic =
+                new Dictionary<int, int>();
+
+            // Count frequency
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (dic.ContainsKey(arr[i]))
+                {
+                    dic[arr[i]]++;
+                }
+                else
+                {
+                    dic[arr[i]] = 1;
+                }
+            }
+
+            List<int> freq = new List<int>();
+
+            int j = 0;
+
+            while (j < k)
+            {
+                int max = int.MinValue;
+
+                int value = 0;
+
+                foreach (var item in dic)
+                {
+                    if (item.Value > max &&
+                        !freq.Contains(item.Key))
+                    {
+                        max = item.Value;
+
+                        value = item.Key;
+                    }
+                }
+
+                freq.Add(value);
+
+                j++;
+            }
+
+            return freq.ToArray();
+        }
+
+    }
+}
+
