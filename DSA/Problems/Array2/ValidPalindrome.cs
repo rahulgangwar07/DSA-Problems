198b675fed2fd8266b73ffa08fdf267c50ec8cc7
DEII <rahulkurmi97194@gmail.com>
2026-07-10 22:08:01 +0530

    Valid Palindrome

---
+++ DSA/Problems/Array2/ValidPalindrome.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Array
+{
+    internal class ContainsDuplicate
+    {
+        //Input: nums = [1,1,1,3,3,4,3,2,4,2]
+        //Output: true
+        public static bool ContainsDuplicates(int[] arr)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (dic.ContainsKey(arr[i]))
+                    return true;
+
+                dic[arr[i]] = i;
+            }
+
+            return false;
+        }
+
+    }
+}
+
