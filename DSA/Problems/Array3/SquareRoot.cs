561ddca3f9bd0859b4122f6b4277627fd3beb0b5
DEII <rahulkurmi97194@gmail.com>
2026-07-19 20:10:12 +0530

    Square Root Problem

---
+++ DSA/Problems/Array3/SquareRoot.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Array3
+{
+    internal class SquareRoot
+    {
+
+        //int v = int.MaxValue;
+        //int value = 15;
+        //int ans = SquareRoot.Root(value);
+        //Console.WriteLine("Root: " + ans);
+
+        //Output:- 16
+        public static int Root(int value)
+        {
+            if (value < 2)
+            {
+                return value;
+            }
+
+            int low = 1;
+            int high = value;
+            int ans = 0;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                if (mid * mid <= value)
+                {
+                    ans = mid;
+                    low = mid+1;
+                }
+                else
+                {
+                    high = mid-1;
+                }
+            }
+
+            return ans;
+        }
+
+
+    }
+}
