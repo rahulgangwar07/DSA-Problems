708b143b03d7f6e3f54f1c7a98968c28ac9660a6
DEII <rahulkurmi97194@gmail.com>
2026-09-23 21:34:07 +0530

    Single Element in a Sorted Array

---
+++ DSA/Problems/Array4/SingleElementinaSortedArray.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Array3
+{
+    internal class SingleElementinaSortedArray
+    {
+        //Input: nums = [1,1,2,3,3,4,4,8,8]
+        //Output: 2
+
+        public int SingleNonDuplicate(int[] nums)
+        {
+            int start = 0;
+            int end = nums.Length - 1;
+            while (start <= end)
+            {
+                int mid = start + (end - start) / 2;
+                if (mid % 2 == 1)
+                {
+                    mid--;
+                }
+                if (nums[mid] == nums[mid + 1])
+                    start = mid + 2;
+                else
+                    end = mid;
+            }
+            return nums[start];
+        }
+    }
+}
